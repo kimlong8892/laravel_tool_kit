52c3b839f5d1393f051d2b984d3910e5b51e0238
Long Nguyen <long@maruweb.vn>
2023-03-06 16:56:26 +0700

    added backup.sh

---
+++ sh/backup.sh
@@ -0,0 +1,7 @@
+date=$(date '+%Y-%m-%d %H:%M:%S')
+docker exec wordpress_db_server /usr/bin/mysqldump -u admin --password=Admin123 wordpress > app/wordpress/sql.sql
+docker exec laravel_db_server /usr/bin/mysqldump -u admin --password=Admin123 laravel > app/laravel/sql.sql
+mkdir -p backup
+zip -r "backup/$date.zip" app/wordpress/wp-content/uploads app/wordpress/sql.sql app/laravel/public/images_upload  app/laravel/sql.sql
+rm -rf app/wordpress/sql.sql
+rm -rf app/laravel/sql.sql
