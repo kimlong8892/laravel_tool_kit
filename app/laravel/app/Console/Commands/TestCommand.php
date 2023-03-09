ea5f12d0451f622c37832c72222c3406371365ba
longtwokey #php_dev <longshare9201@gmail.com>
2023-03-09 11:41:26 +0700

    added theme wordpress html

--- app/laravel/app/Console/Commands/TestCommand.php
+++ app/laravel/app/Console/Commands/TestCommand.php
@@ -31,19 +31,20 @@ class TestCommand extends Command
         $date = new \DateTime();
         $timestamp = $date->getTimestamp(); // Lấy thời gian hiện tại ở định dạng timestamp
         // Chuỗi JSON yêu cầu API
-        $payload = '{ "query": "query Fetch($page:Int){ productOfferV2( listType: 0, sortType: 2, page: $page, limit: 50 ) { nodes { commissionRate commission price productLink offerLink } } }", "operationName": null, "variables":{ "page":0 } }';
+        $payload = '{"query":"mutation{\n    generateShortLink(input:{originUrl:"https://shopee.v
+n/Apple-Iphone-11-128GB-Local-Set-i.52377417.6309028319",subIds:["s1","s2","s3","s4","s5"]}){\n        shortLink\n    }\n}"}';
 
         // Tính toán giá trị chữ ký
         $accessKey = '17342940062';
         $secretKey = '6ERDUYBANO7VTIBSFNQOAHDTLRU3MRV6';
-        $signature = hash('SHA256', $accessKey . $timestamp . $payload . $secretKey);
+        $signature = hash('sha256', $accessKey . $timestamp . $payload . $secretKey);
 
         $headers = [
             'Content-Type' => 'application/json',
             'Authorization' => 'SHA256 Credential=' . $accessKey . ',Timestamp=' . $timestamp . ',Signature=' . $signature,
         ];
 
-        $test = postApi('https://open-api.affiliate.shopee.vn/graphql', [], $headers);
+        $test = postApi('https://open-api.affiliate.shopee.com.my/graphql', [], $headers);
 
         print_r($test);
 
