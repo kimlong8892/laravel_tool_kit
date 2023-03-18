2974ca7af0f2a3f47a5fbac29bf735c822f9a687
longtwokey #php_dev <longshare9201@gmail.com>
2023-03-18 15:24:37 +0700

    remove code

--- app/laravel/app/Providers/AppServiceProvider.php
+++ app/laravel/app/Providers/AppServiceProvider.php
@@ -2,20 +2,6 @@
 
 namespace App\Providers;
 
-use App\Repositories\Accesstrade\AccesstradeRepository;
-use App\Repositories\Accesstrade\AccesstradeRepositoryInterface;
-use App\Repositories\AccesstradeApi\AccesstradeApiRepository;
-use App\Repositories\AccesstradeApi\AccesstradeApiRepositoryInterface;
-use App\Repositories\Campaign\CampaignRepository;
-use App\Repositories\Campaign\CampaignRepositoryInterface;
-use App\Repositories\Category\CategoryRepository;
-use App\Repositories\Category\CategoryRepositoryInterface;
-use App\Repositories\Coupon\CouponRepository;
-use App\Repositories\Coupon\CouponRepositoryInterface;
-use App\Repositories\CrawlShopee\CrawlShopeeRepository;
-use App\Repositories\CrawlShopee\CrawlShopeeRepositoryInterface;
-use App\Repositories\DownloadVideoYoutube\DownloadVideoYoutubeRepository;
-use App\Repositories\DownloadVideoYoutube\DownloadVideoYoutubeRepositoryInterface;
 use App\Repositories\SocialAccount\SocialAccountRepository;
 use App\Repositories\SocialAccount\SocialAccountRepositoryInterface;
 use Illuminate\Support\Facades\URL;
