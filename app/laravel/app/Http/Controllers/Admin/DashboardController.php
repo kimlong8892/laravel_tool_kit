c4d1359db728a18e0fac76cbee9c6cb5b9de881d
longtwokey #php_dev <longshare9201@gmail.com>
2023-03-19 19:15:35 +0700

    fix Get Conversion Report data

--- app/laravel/app/Http/Controllers/Admin/DashboardController.php
+++ app/laravel/app/Http/Controllers/Admin/DashboardController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class DashboardController extends Controller
 {
     public function index(Request $request): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application {
-        $listConversionReport = getConversionReportShopee();
-        $listConversionReport = $listConversionReport['data']['conversionReport']['nodes'] ?? [];
+        $listConversionReport = Cache::get('listConversionReport') ?? null;
+
+        if (empty($listConversionReport)) {
+            $listConversionReport = getConversionReportShopee();
+            $listConversionReport = $listConversionReport['data']['conversionReport']['nodes'] ?? [];
+            Cache::put('listConversionReport', $listConversionReport, 60);
+        }
 
         return view('admin.dashboard.index', compact('listConversionReport'));
     }
--- app/laravel/resources/views/admin/dashboard/index.blade.php
+++ app/laravel/resources/views/admin/dashboard/index.blade.php
@@ -1,43 +1,66 @@
 @extends('layouts.admin')
 
 @section('content')
+    <style>
+        table tr th {
+            text-align: center;
+        }
+
+        table * {
+            word-break: break-all;
+        }
+    </style>
+
     <h1>{{ __('Get Conversion Report data') }}</h1>
     <h5>{{ __('Total Commission') }}: {{ formatVnd(array_sum(array_column($listConversionReport, 'totalCommission'))) }} </h5>
     @php
-        $stt = 1;
+        $id = 1;
     @endphp
     <div>
-        <table class="table table-bordered">
+        <table class="table table-bordered table-responsive">
             <tr>
-                <td><b>{{ __('ID') }}</b></td>
-                <td><b>{{ __('Purchase Time') }}</b></td>
-                <td><b>{{ __('Item Detail') }}</b></td>
-                <td><b>{{ __('Status') }}</b></td>
-                <td><b>{{ __('Total Commission') }}</b></td>
+                <th width="5%" nowrap>{{ __('STT') }}</th>
+                <th width="20%" nowrap>{{ __('Purchase Time') }}</th>
+                <th width="65%" nowrap>{{ __('Item detail') }}</th>
+                <th width="5%" nowrap>{{ __('Status') }}</th>
+                <th width="5%" nowrap>{{ __('Total Commission') }}</th>
             </tr>
             @foreach($listConversionReport as $conversionReport)
                 <tr>
-                    <td>{{ $stt++ }}</td>
-                    <td>{{ date("Y-m-d H:i:s", $conversionReport['purchaseTime']) }}</td>
-                    <td>
-                        <table class="table table-bordered">
+                    <td width="5%">{{ $id++ }}</td>
+                    <td width="20%">{{ date("Y-m-d H:i:s", $conversionReport['purchaseTime']) }}</td>
+                    <td width="65%">
+                        <table cellspacing="0px" class="table table-bordered" width="100%">
+                            @php
+                                $idProduct = 1;
+                            @endphp
+
                             @foreach($conversionReport['orders'] as $order)
+                                <tr>
+                                    <th width="5%" nowrap>{{ __('STT') }}</th>
+                                    <th width="55%" nowrap>{{ __('Item name') }}</th>
+                                    <th width="10%" nowrap>{{ __('Item price') }}</th>
+                                    <th width="10%" nowrap>{{ __('Item commission') }}</th>
+                                    <th width="10%" nowrap>{{ __('Item qty') }}</th>
+                                    <th width="10%" nowrap>{{ __('Item image') }}</th>
+                                </tr>
                                 @foreach($order['items'] as $item)
                                     <tr>
-                                        <td>{{ $item['itemName'] ?? '' }}</td>
-                                        <td><b>{{ __('Item Price') }}:</b> {{ formatVnd($item['itemPrice'] ?? '') }}</td>
-                                        <td><b>{{ __('Item Commission') }}:</b> {{ formatVnd($item['itemCommission'] ?? '') }}</td>
-                                        <td><b>{{ __('Qty') }}:</b> {{ $item['qty'] ?? '' }}</td>
-                                        <td>
-                                            <img src="{{ $item['imageUrl'] ?? '' }}" alt="" width="30%">
+                                        <td width="5%">{{ $idProduct++ }}</td>
+                                        <td width="55%">{{ $item['itemName'] ?? '' }}</td>
+                                        <td width="10%" nowrap>{{ formatVnd($item['itemPrice'] ?? '') }}</td>
+                                        <td width="10%">{{ formatVnd($item['itemCommission'] ?? '') }}</td>
+                                        <td width="10%">{{ $item['qty'] ?? '' }}</td>
+                                        <td width="10%">
+                                            <img src="{{ $item['imageUrl'] ?? '' }}" alt="" width="100%">
                                         </td>
                                     </tr>
                                 @endforeach
                             @endforeach
                         </table>
                     </td>
-                    <td>{{ __($conversionReport['conversionStatus']) }}</td>
-                    <td>{{ formatVnd($conversionReport['totalCommission']) }}</td>
+                    <td width="5%">{{ __($conversionReport['conversionStatus']) }}</td>
+                    <td width="5%">{{ formatVnd($conversionReport['totalCommission']) }}</td>
                 </tr>
             @endforeach
         </table>
