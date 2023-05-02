354c20d39a6c8481447f75f788cb83dce777337e
longtwokey #php_dev <longshare9201@gmail.com>
2023-05-02 23:18:49 +0700

    update home page

--- app/laravel/resources/views/web/home/index.blade.php
+++ app/laravel/resources/views/web/home/index.blade.php
@@ -3,6 +3,14 @@
 @section('title', __('Home page'))
 
 @section('content')
+    <div class="row">
+        @foreach($listPost as $item)
+            @include('web.post.include.post_item_in_list', ['item' => $item])
+        @endforeach
+    </div>
+    <div>
+        {{ $listPost->appends(request()->input())->links() }}
+    </div>
 @endsection
 
 
